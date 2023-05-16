d5f5bd0579be7f75800357909a588e84d4262c77
Victor Lucio (Lucitar) <victorlu3391@gmail.com>
2023-05-15 22:23:47 -0300

    Corrigindo o código

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ valor = int(input())
 
 if valor < 0:
     print("Impossível!")
-if valor < 18:
+elif valor < 18:
     print("Não precisa se alistar.")
 elif valor < 65 and valor > 18:
     print("Não esqueça de votar na próxima eleição.")
